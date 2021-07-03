9f4eff06cabbe6edbc75e98413d0ebdae827874c
Dharmanand <kumar.kd20@gmail.com>
2021-07-03 09:56:20 +0530

    Program to find square Root of a no

---
+++ src/com/kd/SquareRoot.java
@@ -0,0 +1,23 @@
+package com.kd;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(square(25));
+	}
+
+	public static double square(double number){
+	    double t;
+	  
+	    double squareroot = number / 2;
+	  
+	    do {
+	        t = squareroot;
+	        squareroot = (t + (number / t)) / 2;
+	    } while ((t - squareroot) != 0);
+	  
+	    return squareroot;
+	}
+	
+}
